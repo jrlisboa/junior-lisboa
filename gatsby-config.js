e15b5157ec025e1e8c87cb68bd2424121921ee27
José Lisboa <jose.lisboa@willbank.com.br>
2023-05-17 18:51:14 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Junior Lisboa`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
